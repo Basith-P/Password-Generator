e2cc66dc89637e3a4c2bc365a289ad1d91d738c2
Basith P <basithpofficial@gmail.com>
2022-10-04 17:21:13 +0530

    Ask user confirmation before saving data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -11,13 +12,22 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        email_entry.delete(0, END)
-        email_entry.insert(0, "@gmail.com")
-        password_entry.delete(0, END)
-        website_entry.focus()
+    if website == "" or password == "":
+        messagebox.showwarning(
+            title="Oops", message="Please fill the empty fields")
+        return
+
+    is_ok = messagebox.askokcancel(
+        title=website, message=f"Email: {email}\nPassword: {password}\n\nThese credentials will be saved")
+
+    if is_ok:
+        with open("data.txt", "a") as data_file:
+            data_file.write(f"{website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            email_entry.insert(0, "@gmail.com")
+            password_entry.delete(0, END)
+            website_entry.focus()
 
         # ---------------------------- UI SETUP ------------------------------- #
 
