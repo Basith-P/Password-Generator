8aa2d213ae89f0bd56d74d0d87f313cf4f5a3729
Basith P <basithpofficial@gmail.com>
2022-10-04 16:46:23 +0530

    Setup UI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import  *
+from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -13,8 +13,29 @@ window.config(padx=20, pady=20)
 canvas = Canvas(height=200, width=200)
 lock_pic = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_pic)
-canvas.pack()
+canvas.grid(column=1, row=0)
 
+# Labels
+website_label = Label(text="Website: ")
+website_label.grid(row=1, column=0, sticky="w")
+email_label = Label(text="Email: ")
+email_label.grid(row=2, column=0, sticky="w")
+password_label = Label(text="Password: ")
+password_label.grid(row=3, column=0, sticky="w")
+
+# Entries
+website_entry = Entry(width=55)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="w")
+email_entry = Entry(width=55)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="w")
+password_entry = Entry(width=35)
+password_entry.grid(row=3, column=1, sticky="w")
+
+# Buttons
+generate_btn = Button(text="Generate Password")
+generate_btn.grid(row=3, column=2, sticky="e")
+add_btn = Button(text="add", width=46)
+add_btn.grid(row=4, column=1, columnspan=2, sticky="w")
 
 
 window.mainloop()
